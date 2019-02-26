b9358aa987d4dda310ced510cd1b104387bb1f2e
ZazilSosa124 <zazil.s.2023@ceibaprep.org>
2019-02-26 13:24:48 -0800

    Email and Google Login

--- app.js
+++ app.js
@@ -1,8 +1,60 @@
 (function() {
     
-    //Initialize Firebase
+    //TODO: Add the initialization snippet
 
-    firebase.initializeApp(config);
 
-    
+firebase.initializeApp(config);
+
+//Login with Google
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider).then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+  });
+
+//Login with Email and Password
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+const btnSignUp = document.getElementById('btnSignUp');
+const btnLogout = document.getElementById('btnLogout');
+
+
+btnLogin.addEventListener('click', e => { 
+    const email = txtEmail.Value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    const promise = auth.signInWithEmailAndPassword(email, pass)
+    promise.catch(e => console.log(e.message));
+});
+
+btnSignUp.addEventListener('click', e => {
+    const email = txtEmail.Value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    const promise = auth.createUserWithEmailAndPassword(email, pass)
+    promise.catch(e => console.log(e.message));
+});
+
+btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+});
+
+firebase.auth().onAuthStateChanged(firebaseUser =>  {
+    if(firebaseUser) {
+        console.log(firebaseUser);
+        btnLogout.classList.remove('hide');
+    } else {
+        console.log('not logged in');
+        btnLogin.classList.add('hide');
+    }
 })
+
+}());
