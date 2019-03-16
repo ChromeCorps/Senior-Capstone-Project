2a175918f0b9ae857b411b51369b7f62a8d3dc72
Dr.DeLeon <eduardodeleonvalasco@gmail.com>
2019-03-15 19:28:52 -0700

    App Update

--- public/index.js
+++ public/index.js
@@ -1,3 +1,26 @@
+var index ={};
+
+(function(){
+	var firebase = app_fireBase;
+var uid = null;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+		  // User is signed in.
+		  uid = user.uid;
+        }else{
+			// Redirect to login page
+			uid = null;
+			window.location.replace("login.html");
+		}
+	  });
+	
+	function logOut() {
+		firebase.auth().sigOut();
+	}
+
+	index.logOut = logOut;
+})()
+
 $(document).ready(function() {
 	$('.nav-trigger').click(function() {
 		$('.side-nav').toggleClass('visible');
